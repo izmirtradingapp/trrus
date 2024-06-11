534aa08471ec32ac7387c88d07b946b0932af003
izmirtradingapp <114510421+izmirtradingapp@users.noreply.github.com>
2024-06-11 19:34:48 +0300

    Add files via upload

---
+++ 0_👋Hoşgeldin.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(
+    page_title="Hoşgeldin",
+    page_icon="👋",
+)
+
+#st.image("https://t4.ftcdn.net/jpg/05/61/45/97/360_F_561459737_ch2ANlUQeGljLIXTnGrKLCaeTsk8KToB.jpg",use_column_width="always")
+
+
+
+
+
